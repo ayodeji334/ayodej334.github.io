93cc14e4f06e416053eb2581236d082de243047c
ayodeji.334 <ojfawumi@student.edu.ng>
2021-06-17 14:10:24 +0100

    add sidenav

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-jQuery(document).ready(() => {  
+$(document).ready(function(){  
     $("#toggleThemeBtn").click(function(){
         const bgColor = window.getComputedStyle(document.body, null).getPropertyValue('background-color'); 
             if(bgColor === "rgb(255, 255, 255)" || bgColor === "rgba(0, 0, 0, 0)"){
@@ -14,10 +14,15 @@ jQuery(document).ready(() => {
                     "color": "rgb(255,255,255)"
                 });
                 $(this).html('&#9728').css({
-                        "backgroundColor": "rgb(0, 0, 0)",
-                        "color": "rgb(255, 255, 255)"
+                    "backgroundColor": "rgb(0, 0, 0)",
+                    "color": "rgb(255, 255, 255)"
                 });
-
+                $('.menu-btn').css({
+                    "backgroundColor": "rgb(0, 0, 0)"
+                });
+                $('.bar').css({
+                    "backgroundColor": "rgb(255, 255, 255)"
+                })
                 if(Math.floor($(window).scrollTop()) > 60){
                     $('.navbar-container').removeClass("lightBg").addClass("darkBg");
                 }  
@@ -37,12 +42,24 @@ jQuery(document).ready(() => {
                 "backgroundColor": "rgb(255, 255, 255)",
                 "color": "rgb(0, 0, 0)"
             });
+            $('.menu-btn').css({
+                "backgroundColor": "rgb(255, 255, 255)"
+            });
+            $('.bar').css({
+                "backgroundColor": "rgb(0, 0, 0)"
+            })
             if(Math.floor($(window).scrollTop()) > 60){
                 $('.navbar-container').removeClass("darkBg").addClass("lightBg");    
             }
         }
     });
-    $(window).scroll(() => {
+    $(".menu-btn").click(function(){
+        $(".bar1").toggleClass("bar1_transform");
+        $(".bar3").toggleClass("bar3_transform");
+        $(".bar2").toggleClass("bar2_transform");
+        $(".sidenav").toggleClass("open_sidenav");
+    });
+    $(window).scroll(function(){
         const bgColor = window.getComputedStyle(document.body, null).getPropertyValue('background-color');
         const scrollHeight = Math.floor($(window).scrollTop());
         if(scrollHeight === 0 || scrollHeight < 60){
@@ -57,7 +74,19 @@ jQuery(document).ready(() => {
             }else{
                 $(".navbar-container").addClass("darkBg");
             }
-        }
+        };
+
+        // Fade in effect
+        // $("section").each(function(i){
+        //     const bottom_of_item = $(this).position().top() + $(this).outerHeight();
+        //     const bottom_of_window = $(window).scrollTop() + $(window).height();
+
+        //     if(bottom_of_window > bottom_of_item){
+        //         $(this).animate({
+        //             "opacity": "1"
+        //         }, 1000);
+        //     };
+        // });
     });
 });
 
